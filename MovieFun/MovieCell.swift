37177584fe8d5fc3293117ab0aa12971e65dca0e
Ali Daho <larbidahoa@gmail.com>
2021-09-13 23:47:14 -0400

    Adding files

---
+++ MovieFun/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  MovieFun
+//
+//  Created by Ali Daho on 9/13/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
